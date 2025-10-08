b6c06905778d14c0661fa348ab815416b40eb7f1
MotingLiuu <moting.liuu@gmail.com>
2025-10-08 11:25:10 +0900

    added a test bash script

---
+++ research/alignment/sft/test.sh
@@ -0,0 +1,14 @@
+python sft.py \
+    --model_name_or_path Qwen/Qwen2-0.5B \
+    --dataset_name trl-lib/Capybara \
+    --learning_rate 2.0e-5 \
+    --num_train_epochs 1 \
+    --packing \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 8 \
+    --gradient_checkpointing \
+    --eos_token '<|im_end|>' \
+    --eval_strategy steps \
+    --eval_steps 100 \
+    --output_dir Qwen2-0.5B-SFT \
+    --push_to_hub
