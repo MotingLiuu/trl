ecd1c040453faafc3140124770b14d54b1e89219
MotingLiuu <moting.liuu@gmail.com>
2025-10-14 15:51:26 +0900

    added run_config1.sh

---
+++ research/alignment/sft/run_sft_config1.sh
@@ -0,0 +1,7 @@
+export CUDA_VISIBLE_DEVICES=\
+GPU-ff2dcf36-dfcb-be0e-379a-001bcc30512b,\
+GPU-e556ac72-1ce5-119f-b4ba-678c733c7246,\
+GPU-5960f4b8-e49b-524c-2cb4-3cdbcf53ac44,\
+GPU-0acbf3ce-5774-e5d6-837f-db40617b7028
+python sft.py --config config1_1.7B.yaml 
+
